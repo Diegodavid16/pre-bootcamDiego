6c38073e661d665628f55fbe0b20637ab8ea0f54
Diegodavid16 <diegoprogramacion16@gmail.com>
2022-08-12 12:37:44 -0400

    Desafío de código: Predecir funciones

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # pre-bootcamDiego
   git config --global user.email "diegoprogramacion16@gmail.com"
   git config --global user.name "Diegodavid16"
+<ol>
+    <li>
+        <a href="home.html">Inicio</a>
+    </li>
+    <li>
+        <a href="about.html">Sobre Nosotros</a>
+    </li>
+    <li>
+        <a href="contact_us.html">Contáctanos</a>
+    </li>
+</ul>
---
+++ ordoñez_diego_JSPrediccion.js
@@ -0,0 +1,22 @@
+function myBirthYearFunc(){
+    console.log("Nací en " + 1980);
+}copy
+// el console.log imprime nací en 1980
+function myBirthYearFunc(){
+    console.log("Nací en " + 1980);
+}copy
+function myBirthYearFunc(EntradaAñoNacimiento){
+        console.log("Nací en " + EntradaAñoNacimiento);
+    }
+// el console.log imprime naci en 1980
+function add(num1, num2){
+        console.log("¡Sumando números!");///
+        console.log("num1 is: " + num1);
+        console.log("num2 is: " + num2);
+        var sum = num1 + num2;
+        console.log(sum);
+    }
+    //¡Sumando números!
+    //num1 is:10
+    //num2 is:20
+    //el console.log imprime 30    
