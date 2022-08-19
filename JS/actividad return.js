544044ded927eda507622492d37fec466b4e78fc
Diegodavid16 <diegoprogramacion16@gmail.com>
2022-08-19 13:11:29 -0400

    asignacion return

--- JS/actividad return.js
+++ JS/actividad return.js
@@ -1,19 +1,3 @@
-# pre-bootcamDiego
-  git config --global user.email "diegoprogramacion16@gmail.com"
-  git config --global user.name "Diegodavid16"
-<ol>
-    <li>
-        <a href="home.html">Inicio</a>
-    </li>
-    <li>
-        <a href="about.html">Sobre Nosotros</a>
-    </li>
-    <li>
-        <a href="contact_us.html">Contáctanos</a>
-    </li>
-</ul>
-
-
 
 
 
@@ -24,8 +8,10 @@ function hello() {
 var result = hello();
 console.log('result is', result);
 
- hello
- result is 15
+ //hello
+ //result is 15
+
+ /**/
 
  
  
@@ -33,24 +19,36 @@ console.log('result is', result);
     console.log('num is', num);
     return num+15;
 }
+
+
 var result = numPlus(3);
 console.log('result is', result);
 
-var result = num(3) + 15 = 18
-console.log 18
+//var result = num(3) + 15 = 18
+//console.log 18
 
 
 
 
 var num = 15;
+
 console.log(num);
+
 function logAndReturn(num2){
    console.log(num2);   
    return num2;
 }
 var result = logAndReturn(10);
+
+
 console.log(result);
+
 console.log(num);
 
-console.log 15
+//declara una variable num = 15;
+//Muestra la variable num = 15;
+//Creamos una funcion que muestra el dato ingresado.
+//Asignamos a la variable "result" la funcion llamada "logAndReturn".
+//luego mediante console.log llamamos a la funcion.
+//y nos mostrata 10 y 15.
 
